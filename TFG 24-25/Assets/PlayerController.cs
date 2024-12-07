33d717178cb44718331768b1e90f4d1a5b39fac6
Serxres <s.gonzalezpons@gmail.com>
2024-12-07 19:28:27 +0100

    In game menu

--- TFG 24-25/Assets/PlayerController.cs
+++ TFG 24-25/Assets/PlayerController.cs
@@ -6,6 +6,7 @@ using Photon.Pun;
 public class PlayerController : MonoBehaviourPun
 {
     public GameObject playerCamera;
+    public GameObject menu;
 
     void Start()
     {
@@ -17,6 +18,22 @@ public class PlayerController : MonoBehaviourPun
         {
             playerCamera.SetActive(true);
         }
+
+        if (menu != null)
+        {
+            menu.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (menu != null)
+            {
+                bool isActive = menu.activeSelf;
+                menu.SetActive(!isActive);
+            }
+        }
     }
 }
-
