10f7c62b3458e187af81aae105e16f8622dbe0b2
Miquel <miquel.forcada@enti.cat>
2024-12-08 19:22:55 +0100

    Incremented Collider to Grab Token

--- TFG 24-25/Assets/Scripts/Tile.cs
+++ TFG 24-25/Assets/Scripts/Tile.cs
@@ -9,10 +9,10 @@ public class Tile : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Token")
+        if (other.gameObject.tag == "TileCollider")
         {
             Debug.Log("ENTER: " + tileId);
-            other.GetComponent<SelectToken>().SetOnTileId(tileId);
+            other.transform.parent.GetComponent<SelectToken>().SetOnTileId(tileId);
         }
     }
 }
