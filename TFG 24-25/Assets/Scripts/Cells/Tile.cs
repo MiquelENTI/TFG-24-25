8e6d869049a3b87c10b71f727be7084652a2ef65
Miquel <miquel.forcada@enti.cat>
2025-03-14 13:43:29 +0100

    Change Highlight Material When Hovered by a Token

--- TFG 24-25/Assets/Scripts/Cells/Tile.cs
+++ TFG 24-25/Assets/Scripts/Cells/Tile.cs
@@ -7,26 +7,43 @@ public class Tile : MonoBehaviour
 {
     public int tileId;
 
+
+    GameObject tileHighlight;
+    [SerializeField] Material nonSelectedMaterial;
+    [SerializeField] Material selectedMaterial;
+
+    private void Start()
+    {
+        tileHighlight = transform.GetChild(0).gameObject;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         // Token has an emptyObject with collision and its tag is TileCollider
         if (other.gameObject.tag == "TileCollider")
         {
             other.transform.parent.GetChild(0).GetComponent<TokenGameObject>().SetOnTileId(tileId);
+            tileHighlight.GetComponent<Renderer>().material = selectedMaterial;
         }
         // Cards on Hand have an emptyObject with collision and its tag is SpawnTileCollider
         else if (other.gameObject.tag == "SpawnTileCollider")
         {
             other.transform.parent.GetChild(0).rotation = Quaternion.Euler(90, 0, 0);
             other.transform.parent.GetChild(0).GetComponent<CardGameObject>().SetOnTileId(tileId);
+            tileHighlight.GetComponent<Renderer>().material = selectedMaterial;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "SpawnTileCollider")
+        if (other.gameObject.tag == "TileCollider") 
+        {
+            tileHighlight.GetComponent<Renderer>().material = nonSelectedMaterial;
+        }
+        else if (other.gameObject.tag == "SpawnTileCollider")
         {
             other.transform.parent.GetChild(0).rotation = Quaternion.Euler(0, 0, 0);
+            tileHighlight.GetComponent<Renderer>().material = nonSelectedMaterial;
         }
     }
 }
