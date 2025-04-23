9fd8374d9e1e5ba6947187fe5340f85fbc622183
Miquel <miquel.forcada@enti.cat>
2025-04-23 17:19:57 +0200

    Imported Particles

---
+++ TFG 24-25/Assets/Scripts/Managers/EffectsManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectsManager : Singleton<EffectsManager>
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
