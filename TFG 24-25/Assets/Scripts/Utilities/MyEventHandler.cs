629ac7bd2e62ed121145bafb6130b2f54a7df84f
Miquel <miquel.forcada@enti.cat>
2025-04-25 19:07:42 +0200

    Removed Old Samurai Code, Implemented New Samurai

--- TFG 24-25/Assets/Scripts/Utilities/MyEventHandler.cs
+++ TFG 24-25/Assets/Scripts/Utilities/MyEventHandler.cs
@@ -21,9 +21,6 @@ public class MyEventHandler : Singleton<MyEventHandler>
         moveToken = new UnityEvent<int, int>();
         spawnToken = new UnityEvent<int, int, bool>();
 
-        samuraiEffectRed = new();
-        samuraiEffectBlue = new();
-
         moveToken.AddListener((int cellId, int characterId) =>
         {
             photonView.RPC("RPC_MoveToken", RpcTarget.AllBuffered, cellId, characterId);
@@ -72,71 +69,6 @@ public class MyEventHandler : Singleton<MyEventHandler>
     public void RPC_SpawnToken(int cellId, int characterId, bool bypassSpawn)
     {
         InvokeSpawnToken(cellId, characterId, bypassSpawn);
-    }
-
-    public void StartSamuraiEffect(TeamType teamType)
-    {
-        switch (teamType)
-        {
-            case TeamType.RED:
-                {
-                    samuraiEffectRed.AddListener(() =>
-                    {
-                        photonView.RPC("RPC_ActivateSamuraiEffect", RpcTarget.All, teamType);
-                        Debug.Log("SAMURAI RED ACTIVATED");
-                    });
-                    break;
-                }
-            case TeamType.BLUE:
-                samuraiEffectBlue.AddListener(() =>
-                {
-                    photonView.RPC("RPC_ActivateSamuraiEffect", RpcTarget.All, teamType);
-                    Debug.Log("SAMURAI RED ACTIVATED");
-                });
-                break;
-            default:
-                break;
-        }
-    }
-
-    public void DeactivateSamuraiEffect(TeamType teamType)
-    {
-        switch (teamType)
-        {
-            case TeamType.RED:
-                samuraiEffectRed.RemoveAllListeners();
-                break;
-            case TeamType.BLUE:
-                samuraiEffectBlue.RemoveAllListeners();
-                break;
-        }
-    }    
-
-    [PunRPC]
-    public void RPC_ActivateSamuraiEffect(TeamType teamType)
-    {
-        CharactersManager.Instance.GetCharactersByColor(teamType).ForEach(character =>
-        {
-            if (character.GetCharacterStats().getName() == "Samurai")
-            {
-                character.Effect();
-            }
-        });
-    }
-
-    public void InvokeSamuraiEffect(TeamType teamType)
-    {
-        switch (teamType)
-        {
-            case TeamType.RED:
-                samuraiEffectRed.Invoke();
-                break;
-            case TeamType.BLUE:
-                samuraiEffectBlue.Invoke();
-                break;
-            default:
-                break;
-        }
     }
 }
 
