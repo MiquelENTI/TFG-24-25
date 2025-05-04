c47873887ce198b9589a4f264ae3a39331d24727
Miquel <miquel.forcada@enti.cat>
2025-05-04 21:42:02 +0200

    Fixed EndTurnButton

--- TFG 24-25/Assets/Scripts/RayCastInteraction/RayCastEndTurn.cs
+++ TFG 24-25/Assets/Scripts/RayCastInteraction/RayCastEndTurn.cs
@@ -7,19 +7,9 @@ public class RayCastEndTurn : MonoBehaviour
 {
     TurnManagerScript turnManagerScript;
 
-    [SerializeField] Material interactAvailable;
-    [SerializeField] Material interactUnavailable;
-
-    Renderer r;
-
-    PhotonView photonView;
-
     private void Start()
     {
         turnManagerScript = TurnManagerScript.Instance;
-        photonView = GetComponent<PhotonView>();
-
-        r = GetComponent<Renderer>();
     }
 
     public void Interact(bool isBluePlayer)
@@ -27,24 +17,6 @@ public class RayCastEndTurn : MonoBehaviour
         if (turnManagerScript.GetIsTurnBlue() == isBluePlayer || turnManagerScript.GetTurnBypass())
         {
             turnManagerScript.TurnManager();
-            ChangeMaterial();
-            r.material = interactUnavailable;
         }
-        if (turnManagerScript.GetTurnBypass())
-        {
-            r.material = interactAvailable;
-        }
-    }
-
-    public void ChangeMaterial()
-    {
-        photonView.RPC("ChangeMaterial_RPC", RpcTarget.Others);
-    }
-
-    [PunRPC]
-    public void ChangeMaterial_RPC()
-    {
-        r.material = interactAvailable;
-
     }
 }
