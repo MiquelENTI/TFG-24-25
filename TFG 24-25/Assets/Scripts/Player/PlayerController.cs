6e280d782de1dda6ccea721db6303a21b63e4164
Miquel <miquel.forcada@enti.cat>
2025-05-11 20:42:36 +0200

    Trying New Camera Changes

--- TFG 24-25/Assets/Scripts/Player/PlayerController.cs
+++ TFG 24-25/Assets/Scripts/Player/PlayerController.cs
@@ -15,26 +15,28 @@ public class PlayerController : MonoBehaviourPun
     private Vector3 PC_WizardHeadOffset = new Vector3(0, -0.515f, -0.7f);
     private Vector3 PC_KnightHeadOffset = new Vector3(0, -0.515f, 0.7f);
 
-    private Vector3 VR_WizardHeadOffset;
-    private Vector3 VR_KnightHeadOffset;
+    private Vector3 VR_WizardHeadOffset = new Vector3(0f, -0.155f, -0.25f);
+    private Vector3 VR_KnightHeadOffset = new Vector3(0f, -0.155f, 0.25f);
+
+    bool headCreated = false;
+
+
+    private void Awake()
+    {
+       
+    }
 
     void Start()
     {
         if (!photonView.IsMine)
         {
             playerCamera.SetActive(false);
+            return;
         }
         else
         {
             playerCamera.SetActive(true);
         }
-
-        if (menu != null)
-        {
-            menu.SetActive(false);
-        }
-
-        if (!photonView.IsMine) { return; }
 
         if (TurnManagerScript.Instance.isPCVersion)
         {
@@ -45,23 +47,29 @@ public class PlayerController : MonoBehaviourPun
             }
             else
             {
-                playerHead = PhotonNetwork.Instantiate("KnightHead", playerCamera.transform.position + PC_KnightHeadOffset, Quaternion.Euler(0,180,0));
+                playerHead = PhotonNetwork.Instantiate("KnightHead", playerCamera.transform.position + PC_KnightHeadOffset, Quaternion.Euler(0, 180, 0));
                 playerHead.transform.parent = gameObject.transform.GetChild(0);
             }
-
         }
         else
         {
             if (PhotonNetwork.IsMasterClient)
             {
-                playerHead = PhotonNetwork.Instantiate("WizardHead", Vector3.zero, Quaternion.identity);
+                playerHead = PhotonNetwork.Instantiate("WizardHead", playerCamera.transform.position + VR_WizardHeadOffset, Quaternion.identity);
                 playerHead.transform.parent = gameObject.transform.GetChild(2).GetChild(0).GetChild(0);
             }
             else
             {
-                playerHead = PhotonNetwork.Instantiate("KnightHead", Vector3.zero, Quaternion.identity);
+                playerHead = PhotonNetwork.Instantiate("KnightHead", playerCamera.transform.position + VR_KnightHeadOffset, Quaternion.Euler(0, 180, 0));
                 playerHead.transform.parent = gameObject.transform.GetChild(2).GetChild(0).GetChild(0);
             }
+        }
+
+        headCreated = true;
+
+        if (menu != null)
+        {
+            menu.SetActive(false);
         }
     }
 
