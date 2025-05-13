1bec467630c8dfa99e7c5fce0f6fa9e1ca3805fc
Miquel <miquel.forcada@enti.cat>
2025-05-13 22:48:04 +0200

    Rotated Enemys cards

--- TFG 24-25/Assets/Scripts/Managers/DeckManager.cs
+++ TFG 24-25/Assets/Scripts/Managers/DeckManager.cs
@@ -89,11 +89,26 @@ public class DeckManager : Singleton<DeckManager>
         cardHold.GetComponent<CardHold>().AddCardToHold(instantiatedCard);
         if (PhotonNetwork.IsMasterClient)
         {
-            cardHold.GetComponent<CardHold>().SetDefaultCardRotation(new Vector3(0, 0, 0));
+            if (isBlue)
+            {
+                cardHold.GetComponent<CardHold>().SetDefaultCardRotation(new Vector3(0, 0, 0));
+            }
+            else
+            {
+                cardHold.GetComponent<CardHold>().SetDefaultCardRotation(new Vector3(0, 180, 0));
+            }
+            
         }
         else 
         {
-            cardHold.GetComponent<CardHold>().SetDefaultCardRotation(new Vector3(0, 180, 0));
+            if (isBlue)
+            {
+                cardHold.GetComponent<CardHold>().SetDefaultCardRotation(new Vector3(0, 180, 0));
+            }
+            else
+            {
+                cardHold.GetComponent<CardHold>().SetDefaultCardRotation(new Vector3(0, 0, 0));
+            }
         }
         
     }
